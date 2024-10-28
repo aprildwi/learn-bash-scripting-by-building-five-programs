7ad92c828e518c13476bbd86d32e59df2ca9c2a1
april dwi <dwiaprilia987@gmail.com>
2024-10-28 07:36:41 +0000

    Save progress: 620.1

--- countdown.sh
+++ countdown.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 # Program that counts down to zero from a given argument
-if [[ $1 == arg1 ]]
-then
-echo true
-fi
-if [[ $1 -gt 0 ]]
-then
-  echo true
-else
-  echo false
-fi
 if [[ $1 -gt 0 ]]
 then
   for (( i = $1; i > 0; i-- ))
