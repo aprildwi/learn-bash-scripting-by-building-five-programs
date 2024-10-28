2eb1a8476c49092d0e14018b95a0dff60415918d
april dwi <dwiaprilia987@gmail.com>
2024-10-28 07:58:33 +0000

    APRILIA.DWIYANI_2404005

--- bingo.sh
+++ bingo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=$(( RANDOM % 15 + 1 ))
+NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
 TEXT="The next number is, " 
 if (( NUMBER <= 15 ))
