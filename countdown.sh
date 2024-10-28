9241370e18f710d952fb2090112ceacafb8cb1ff
april dwi <dwiaprilia987@gmail.com>
2024-10-28 07:39:44 +0000

    Save progress: 692.1

--- countdown.sh
+++ countdown.sh
@@ -2,11 +2,11 @@
 # Program that counts down to zero from a given argument
 if [[ $1 -gt 0 ]]
 then
-  for (( i = $1; i > 0; i-- ))
+  for (( i = $1; i >= 0; i-- ))
   do
     echo $i
+    sleep 1
   done
 else
   echo Include a positive integer as the first argument.
 fi
-sleep 1
